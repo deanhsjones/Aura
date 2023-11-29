379f68d21b72cc6dd9a64b24705e24048f313d51
Dean Jones <84123704+deanhsjones@users.noreply.github.com>
2023-11-30 00:09:34 +0100

    Added player controller class and BP

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -1,12 +1,12 @@
 // Copyright Dean Jones
 
-#pragma once
-#include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
 
 class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
 /**
  *
@@ -21,7 +21,14 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	virtual void SetupInputComponent() override;
+
 private:
 	UPROPERTY(EditAnywhere, Category = "Input")
 		TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+		TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
