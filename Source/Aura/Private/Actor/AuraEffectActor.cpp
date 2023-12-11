03b4070a4bebd5faae5249a7c0222f2ab405e0b1
Dean Jones <84123704+deanhsjones@users.noreply.github.com>
2023-12-11 18:44:34 +0100

    Completed implementation of instant DP effects
    Finished making Mana and health potions with Gameplay Effects and exposed values

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -5,54 +5,16 @@
 #include "AbilitySystemBlueprintLibrary.h"
 
 #include "AbilitySystemComponent.h"
-//#include "AbilitySystemInterface.h"
-//#include "AbilitySystem/AuraAttributeSet.h"
-//
-//#include "Components/SphereComponent.h"
+
 
 
 AAuraEffectActor::AAuraEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	//Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	//SetRootComponent(Mesh);
-	//Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	//Sphere->SetupAttachment(GetRootComponent());
-
 	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 	
 }
-
-//void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-//	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-//{
-//
-//	//TO DO: Change this to apply a Gameplay Effect; for now using const cast as a hack just to see how it works ;)
-//
-//	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-//	{
-//		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
-//		//here is the const cast! This is dangerous and should be changed!
-//		UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
-//		MutableAuraAttributeSet->SetHealth(AuraAttributeSet->GetHealth() + 25.f);
-//		MutableAuraAttributeSet->SetMana(AuraAttributeSet->GetMana() - 25.f);
-//		//that was the const cast! Change it later!
-//		Destroy();
-//	}
-//}
-//
-//void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
-//	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-//{
-//}
-
-//void AAuraEffectActor::BeginPlay()
-//{
-//	//Super::BeginPlay();
-//	//Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
-//	//Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
-//}
 
 void AAuraEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -7,7 +7,6 @@
 #include "AuraEffectActor.generated.h"
 
 
-//class USphereComponent;
 class UGameplayEffect;
 
 
@@ -19,30 +18,19 @@ class AURA_API AAuraEffectActor : public AActor
 public:	
 	AAuraEffectActor();
 
-	//UFUNCTION()
-	//virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
-	//	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	//UFUNCTION()
-	//virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
-	//	UPrimitiveComponent* OtherComp,	int32 OtherBodyIndex);
 
 
 
 protected:
-	/*virtual void BeginPlay() override;*/
 
 	UFUNCTION(BlueprintCallable)
-	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
-	UPROPERTY(EditAnywhere, Category = "Applied Effects")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
 private:
-	//UPROPERTY(VisibleAnywhere)
-	//TObjectPtr<USphereComponent> Sphere;
 
-	//UPROPERTY(VisibleAnywhere)
-	//TObjectPtr<UStaticMeshComponent> Mesh;
 
 };
