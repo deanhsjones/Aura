70c26bdd00386076ffacb9d3cca32177d602c680
Dean Jones <84123704+deanhsjones@users.noreply.github.com>
2024-01-11 15:39:37 +0100

    Ghost Globe
    Set up an interpolating progress bar for vital attributes that lags slightly behind current attribute level

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -13,10 +13,6 @@ void UOverlayWidgetController::BroadcastInitialValues()
 	OnMaxHealthChanged.Broadcast(AuraAttributeSet->GetMaxHealth());
 	OnManaChanged.Broadcast(AuraAttributeSet->GetMana());
 	OnMaxManaChanged.Broadcast(AuraAttributeSet->GetMaxMana());
-
-	
-
-
 }
 
 void UOverlayWidgetController::BindCallbacksToDependencies()
@@ -59,13 +55,7 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 		[this](const FGameplayTagContainer& AssetTags)
 		{
 			for (const FGameplayTag& Tag : AssetTags)
-			{
-
-				// "A.1".MatchesTag("A") will return True, "A".MatchesTag("A.1") will return False
-				// 
-				// For example, Tag = Message.HealthPotion
-				// "Message.HealthPotion".MatchesTag("Message") will return True, "Message".MatchesTag("Message.HealthPotion") will return False
-
+			{								
 				FGameplayTag MessageTag = FGameplayTag::RequestGameplayTag(FName("Message"));
 				if (Tag.MatchesTag(MessageTag))
 				{
@@ -78,22 +68,4 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 	
 }
 
-//void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
-//{
-//	OnHealthChanged.Broadcast(Data.NewValue);
-//}
 
-//void UOverlayWidgetController::MaxHealthChanged(const FOnAttributeChangeData& Data) const
-//{
-//	OnMaxHealthChanged.Broadcast(Data.NewValue);
-//}
-//
-//void UOverlayWidgetController::ManaChanged(const FOnAttributeChangeData& Data) const
-//{
-//	OnManaChanged.Broadcast(Data.NewValue);
-//}
-//
-//void UOverlayWidgetController::MaxManaChanged(const FOnAttributeChangeData& Data) const
-//{
-//	OnManaChanged.Broadcast(Data.NewValue);
-//}
--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -29,15 +29,7 @@ struct FUIWidgetRow : public FTableRowBase
 class UAuraUserWidget;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAttributeChangedSignature, float, NewValue);
-
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, MaxHealth);
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
-
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
-
-
 
 
 /**
@@ -66,17 +58,10 @@ public:
 	UPROPERTY(BlueprintAssignable, Category = "GAS|Messages")
 		FMessageWidgetRowSignature MessageWidgetRowDelegate;
 
-
 protected:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
 	TObjectPtr<UDataTable> MessageWidgetDataTable;
-
-
-	/*void HealthChanged(const FOnAttributeChangeData& Data) const;*/
-	//void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
-	//void ManaChanged(const FOnAttributeChangeData& Data) const;
-	//void MaxManaChanged(const FOnAttributeChangeData& Data) const;
 
 	template<typename T>
 	T* GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag);
