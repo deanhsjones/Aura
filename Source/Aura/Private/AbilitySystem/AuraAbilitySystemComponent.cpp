7e51dbd8836368274a3b07b4958c4c7ce2a17ed6
Dean Jones <84123704+deanhsjones@users.noreply.github.com>
2024-01-25 14:10:06 +0100

    Custom asset manager
    Created a custom asset manager for the project
    override the StartInitialLoading and include InitialiseNativeGameplayTags
    set assetmanager in engine ini
    created test print for attribute in ASC

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -2,10 +2,19 @@
 
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
 
 void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
+
+	const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
+	//GameplayTags.Attributes_Secondary_Armor.ToString()
+	GEngine->AddOnScreenDebugMessage(-1,
+		10.f,
+		FColor::Orange,
+		FString::Printf(TEXT("Tag: %s"), *GameplayTags.Attributes_Secondary_Armor.ToString())
+	);
 }
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
@@ -14,12 +23,5 @@ void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Ability
 	EffectSpec.GetAllAssetTags(TagContainer);
 
 	EffectAssetTags.Broadcast(TagContainer);
-
-	//for (const FGameplayTag& Tag : TagContainer)
-	//{
-	//	//TODO: Broadcast tag to the widget controller
-	//	const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
-	//	GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
-	//}
 
 }
