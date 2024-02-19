8a7c42a1f4cacd50928c97c8ad2bd43002a6cc6e
Dean Jones <84123704+deanhsjones@users.noreply.github.com>
2024-02-19 11:38:51 +0100

    Created Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Dean Jones
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies() 
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues() 
+{
+
+}
