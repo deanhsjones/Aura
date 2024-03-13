5067aad2e34964d3ad0fd276397f183acc559c95
Dean Jones <84123704+deanhsjones@users.noreply.github.com>
2024-03-13 16:22:47 +0100

    Replicating data up to the server
    Target data is RFC'd to the server and the server the checks to see if the delegate has broadcast and acts appropriately

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -19,7 +19,23 @@ void UTargetDataUnderMouse::Activate()
 	}
 	else
 	{
-		//todo we're on server so listen for target data
+		const FGameplayAbilitySpecHandle SpecHandle = GetAbilitySpecHandle();
+		const FPredictionKey ActivationPredictionKey = GetActivationPredictionKey();
+
+		AbilitySystemComponent.Get()->AbilityTargetDataSetDelegate(
+			SpecHandle,
+			ActivationPredictionKey).AddUObject(
+				this,
+				&UTargetDataUnderMouse::OnTargetDataReplicatedCallback);
+
+		const bool bCalledDelegate = AbilitySystemComponent.Get()->CallReplicatedTargetDataDelegatesIfSet(
+			SpecHandle,
+			ActivationPredictionKey);
+
+		if (!bCalledDelegate)
+		{
+			SetWaitingOnRemotePlayerData();
+		}
 	}
 
 	
@@ -46,6 +62,16 @@ void UTargetDataUnderMouse::SendMouseCursorData()
 		FGameplayTag(),
 		AbilitySystemComponent->ScopedPredictionKey);
 
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
+}
+
+void UTargetDataUnderMouse::OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag)
+{
+	AbilitySystemComponent->ConsumeClientReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey());
+	
 	if (ShouldBroadcastAbilityTaskDelegates())
 	{
 		ValidData.Broadcast(DataHandle);
--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -28,4 +28,7 @@ private:
 
 	virtual void Activate() override;
 	void SendMouseCursorData();
+
+
+	void OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag);
 };
