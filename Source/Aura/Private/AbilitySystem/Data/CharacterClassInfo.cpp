28718ae26b0b43bc07857d4434a4ebcb9e0c4001
Dean Jones <84123704+deanhsjones@users.noreply.github.com>
2024-03-20 15:20:49 +0100

    RPG Character Classes
    creating data assets for character classes to initialise attributes by character class

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Dean Jones
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
